e6c0e9697fd823a256f3b060143afbf61ffbc5b1
vishnuawasthi <vishnuawasthi121@gmail.com>
2019-09-22 16:17:30 +0530

    Added exclude=HibernateJpaAutoConfiguration.class
    as the solution of
    page from request [/client-config] due to exception
    [org.springframework.orm.jpa.EntityManagerHolder cannot be cast to o
    rg.springframework.orm.hibernate5.SessionHolder]
    
    java.lang.ClassCastException:
    org.springframework.orm.jpa.EntityManagerHolder cannot be cast to
    org.springframework.orm.
    hibernate5.SessionHolder
            at org.springframework.orm.hibernate5.HibernateTransactionManager.doGetTransaction(HibernateTransactionManager.j
    ava:377) ~[spring-orm-4.3.3.RELEASE.jar:4.3.3.RELEASE]

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -2,9 +2,10 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
